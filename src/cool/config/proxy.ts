12d844a53b424172341e396c508841a830018015
homend <gx_eipsycongroo@126.com>
2023-11-13 17:54:15 +0800

    [config] 优化配置信息

--- src/cool/config/proxy.ts
+++ src/cool/config/proxy.ts
@@ -1,12 +1,26 @@
+// import { loadEnv } from 'vite'
+
+// const env = loadEnv(process?.env?.NODE_ENV || '', process.cwd(), 'VITE_')
+// const targetUrl = env.VITE_APP_SERVER_TARGET
+
+const targetUrl = 'http://127.0.0.1:8001'
+
 export const proxy = {
 	"/dev/": {
-		target: "http://127.0.0.1:8001",
+		target: targetUrl,
+		changeOrigin: true,
+		rewrite: (path: string) => path.replace(/^\/dev/, "")
+	},
+
+	// 文件下载
+	"/public/uploads/": {
+		target: targetUrl,
 		changeOrigin: true,
 		rewrite: (path: string) => path.replace(/^\/dev/, "")
 	},
 
 	"/prod/": {
-		target: "http://127.0.0.1:8001",
+		target: targetUrl,
 		changeOrigin: true,
 		rewrite: (path: string) => path.replace(/^\/prod/, "/api")
 	}
